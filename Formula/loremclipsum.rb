aadf9437858fd4045c34d5a2fb2b3c57a42f8ccb
404answernotfound <404answernotfound.public@gmail.com>
2023-05-27 16:50:17 +0200

    Fixed Foruma SHA-256 for release v1.0.0

--- Formula/loremclipsum.rb
+++ Formula/loremclipsum.rb
@@ -1,7 +1,7 @@
 class Loremclipsum < Formula
     desc "A CLI tool to copy-paste lorem ipsum text in different flavors."
     homepage "https://github.com/Schrodinger-Hat/loremclipsum"
-    url "https://github.com/Schrodinger-Hat/loremclipsum/blob/main/loremclipsum.tar.gz"
+    url "https://github.com/Schrodinger-Hat/loremclipsum/archive/refs/tags/v1.0.0.tar.gz"
     sha256 "e5d45e61bea682388276d688ebffd52ebbaa9f82494d1d7317aeebb54fde5d9c"
     version "1.0.0"
     license "MIT"
