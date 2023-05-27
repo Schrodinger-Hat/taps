2b2c49961c1a6c1ae38756d722f258e6d8c35671
404answernotfound <404answernotfound.public@gmail.com>
2023-05-27 17:08:14 +0200

    Fixed formula with new url

--- Formula/loremclipsum.rb
+++ Formula/loremclipsum.rb
@@ -1,16 +1,16 @@
 class Loremclipsum < Formula
     desc "A CLI tool to copy-paste lorem ipsum text in different flavors."
     homepage "https://github.com/Schrodinger-Hat/loremclipsum"
-    url "https://github.com/Schrodinger-Hat/loremclipsum/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "e5d45e61bea682388276d688ebffd52ebbaa9f82494d1d7317aeebb54fde5d9c"
+    url "https://github.com/Schrodinger-Hat/loremclipsum/blob/main/loremclipsum.tar.gz"
+    sha256 "bd0afac00e4773284c55a5b774021b966129c3e298c2c220652dbbb421dff4cb"
     version "1.0.0"
     license "MIT"
   
     def install
-        system "tar", "xf", "loremclipsum-#{version}.tar.gz"
-        
+        system "tar", "xf", "loremclipsum.tar.gz"
+
         # Move the extracted files to the appropriate location
-        bin.install "loremclipsum-#{version}/loremclipsum"
+        bin.install "loremclipsum/loremclipsum"
 
         # Make the executable executable
         chmod "+x", bin/"loremclipsum"
