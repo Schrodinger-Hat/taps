958777d9e8a9300234e97b2aa3b2fa523c995c3a
404answernotfound <404answernotfound.public@gmail.com>
2023-05-27 16:48:37 +0200

    Changed formula SHA256

--- Formula/loremclipsum.rb
+++ Formula/loremclipsum.rb
@@ -2,7 +2,7 @@ class Loremclipsum < Formula
     desc "A CLI tool to copy-paste lorem ipsum text in different flavors."
     homepage "https://github.com/Schrodinger-Hat/loremclipsum"
     url "https://github.com/Schrodinger-Hat/loremclipsum/blob/main/loremclipsum.tar.gz"
-    sha256 "20c7ed1b28483d9434f3f01f0568a2813bba9733cc4fa768ff7ce073df5c7fa4"
+    sha256 "e5d45e61bea682388276d688ebffd52ebbaa9f82494d1d7317aeebb54fde5d9c"
     version "1.0.0"
     license "MIT"
   
